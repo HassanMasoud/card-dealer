09d75fe73e4630f9994462156e504de2699afa7c
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-07-28 18:18:00 +0300

    Fetch single card from same deck

--- src/Deck.js
+++ src/Deck.js
@@ -1,25 +1,35 @@
 import React, { Component } from "react";
 import axios from "axios";
-const API_URL = "https://deckofcardsapi.com/api/deck/new/shuffle";
+const BASE_API_URL = "https://deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
   constructor(props) {
     super(props);
 
     this.state = { deck: null };
+    this.getCard = this.getCard.bind(this);
   }
 
   async componentDidMount() {
-    let deck = await axios.get(API_URL);
+    let deck = await axios.get(`${BASE_API_URL}/new/shuffle`);
     this.setState({
       deck: deck.data,
     });
+  }
+
+  async getCard() {
+    const deck_id = this.state.deck.deck_id;
+    const cardUrl = `${BASE_API_URL}/${deck_id}/draw/`;
+    const cardRes = await axios.get(cardUrl);
+    const card = cardRes.data.cards[0];
+    console.log(card);
   }
 
   render() {
     return (
       <div>
         <h1>Card Dealer</h1>
+        <button onClick={this.getCard}>Get Card</button>
       </div>
     );
   }
